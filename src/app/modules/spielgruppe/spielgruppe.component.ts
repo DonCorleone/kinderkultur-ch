4acc2fd43fe00c54d3f9526d0bc186ef17756537
Don Corleone <vitocorleone77@gmail.com>
2022-03-24 23:16:44 +0100

    back mechanism

--- src/app/modules/spielgruppe/spielgruppe.component.ts
+++ src/app/modules/spielgruppe/spielgruppe.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-spielgruppe',
   templateUrl: './spielgruppe.component.html',
-  styleUrls: ['./spielgruppe.component.scss']
+  styleUrls: ['./spielgruppe.component.scss'],
 })
-export class SpielgruppeComponent {}
-
-
+export class SpielgruppeComponent {
+  constructor(private location: Location) {}
+  back(): void {
+    this.location.back();
+  }
+}
