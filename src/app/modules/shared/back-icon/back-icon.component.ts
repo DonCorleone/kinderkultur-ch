657d7b1971b7aa790df484f09dc099c8702cd27e
Don Corleone <vitocorleone77@gmail.com>
2022-03-27 11:40:06 +0200

    back icon styled

--- src/app/modules/shared/back-icon/back-icon.component.ts
+++ src/app/modules/shared/back-icon/back-icon.component.ts
@@ -1,8 +1,10 @@
-import { Component } from '@angular/core';
+import {Component, HostBinding, Input} from '@angular/core';
 
 @Component({
   selector: 'app-back-icon',
   templateUrl: './back-icon.component.html',
   styleUrls: ['./back-icon.component.scss'],
 })
-export class BackIconComponent {}
+export class BackIconComponent {
+  @Input() @HostBinding('class.spielgruppe-bg-color') public isSpielgruppe = false;
+}
